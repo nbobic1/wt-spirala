0b05c3f4a0292a6b1f97fe789cb7a6d170b17dcf
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2022-11-13 20:24:45 +0100

    initial commit

---
+++ scripts/TabelaPrisustvo.js
@@ -0,0 +1,20 @@
+let TabelaPrisustvo = function (divRef, podaci) {
+    //privatni atributi modula
+    //
+
+    //implementacija metoda
+    let sljedecaSedmica = function () {
+
+    }
+
+    let prethodnaSedmica = function () {
+
+    }
+
+
+    return {
+        sljedecaSedmica: sljedecaSedmica,
+        prethodnaSedmica: prethodnaSedmica
+    }
+};
+ 
