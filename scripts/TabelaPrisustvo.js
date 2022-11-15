013612dfc8252beb15004e52259121cbbdd9eece
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2022-11-15 11:07:36 +0100

    pocetak pravljenja tabele

--- scripts/TabelaPrisustvo.js
+++ scripts/TabelaPrisustvo.js
@@ -6,14 +6,16 @@ let TabelaPrisustvo = function (divRef, podaci) {
     var br_prisustva=podaci.prisustvo.length;
     if(br_prisustva<0||br_prisustva!=podaci.brojPredavanjaSedmicno||br_prisustva!=podaci.brojVjezbiSedmicno)
     {
-
+        divRef.innerHTML="Podaci o prisustvu nisu validni!";
+        return {sljedecaSedmica:null,prethodnaSedmica:null};
     }
     //dva studenta sa istim indeksom
     for(var i=0;i<podaci.studenti.length;i++)
     {
         if(podaci.studenti.filter(e=>e.index==podaci.studenti[i].index).length!=0)
         {
-            
+            divRef.innerHTML="Podaci o prisustvu nisu validni!";
+            return {sljedecaSedmica:null,prethodnaSedmica:null};                
         }
     } 
     //index je u prisustvu ali nije u nizu sa studentima
@@ -21,7 +23,8 @@ let TabelaPrisustvo = function (divRef, podaci) {
     {
         if(podaci.studenti.filter(e=>e.index==podaci.prisustvo[i].index).length==0)
         {
-
+            divRef.innerHTML="Podaci o prisustvu nisu validni!";
+            return {sljedecaSedmica:null,prethodnaSedmica:null};
         }
     }   
     //preskocena sedmica
@@ -31,7 +34,8 @@ let TabelaPrisustvo = function (divRef, podaci) {
     {
         if(k[i]-1!=k[i-1])
         {
-            
+            divRef.innerHTML="Podaci o prisustvu nisu validni!";
+            return {sljedecaSedmica:null,prethodnaSedmica:null};                
         }
     }
     //isti student ima 2 ili više prisustva za istu sedmicu
@@ -39,10 +43,76 @@ let TabelaPrisustvo = function (divRef, podaci) {
     {
         if(podaci.prisustvo.filter(e=>e.index==podaci.prisustvo[i].index&&e.sedmica==podaci.prisustvo[i].sedmica).length!=1)
         {
-
+            divRef.innerHTML="Podaci o prisustvu nisu validni!";
+            return {sljedecaSedmica:null,prethodnaSedmica:null};    
         }
     }
+    //inicijalizacija modula
+    divRef.innerHTML="<table><tr><td><b>Ime i prezime</b></td><td><b>Index</b></td>";
+    for(var i=1;i<Math.max(...k);i++)
+    {
+        divRef.innerHTML+="<td><b>"+rimski(i)+"</b></td>"
+    }
+    divRef.innerHTML+="</tr>"
+    for(var j=0;j<podaci.studenti.length;j++)
+    {
+        divRef.innerHTML+="<tr><td rowspan=\"2\"><b>"+podaci.studenti[j].ime+"</b></td><td rowspan=\"2\"><b>"+podaci.studenti[j].index+"</b></td>"     
+        for(var i=1;i<Math.max(...k);i++)
+        {
 
+        }
+        divRef.innerHTML+="</tr>"
+    }
+    divRef.innerHTML+="</table>"  
+    console.log(div.innerHTML);
+ function rimski(a)
+ {
+    switch(a)
+    {
+        case 1:
+            return "I";
+        break;
+        case 2:
+            return "II";
+        break;
+        case 3:
+            return "III";
+        break;
+        case 4:
+            return "IV";
+        break;
+        case 5:
+            return "V";
+        break;
+        case 6:
+            return "VI";
+        break;
+        case 7:
+            return "VII";
+        break;
+        case 8:
+            return "VIII";
+        break;
+        case 9:
+            return "IX";
+        break;
+        case 10:
+            return "X";
+        break;
+        case 11:
+            return "XI";
+        break;
+        case 12:
+            return "XII";
+        break;
+        case 13:
+            return "XIII";
+        break;
+        case 14:
+            return "XIV";
+        break;
+    }
+ }
     //implementacija metoda
     let sljedecaSedmica = function () {
 
