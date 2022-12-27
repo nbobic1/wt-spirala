ca33baae07d8a6e6d37374da6e2066952a390cd0
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2022-12-27 02:19:21 +0100

    login

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const session = require("express-session");
 const app = express();
 const path=require('path')
+const pug = require('pug');
 //sesija
 app.use(session({
     secret: 'neka tajna sifra',
@@ -21,14 +22,38 @@ app.get('/prisustvo.html',(req,res)=>{
 app.get('/prijava.html',(req,res)=>{
     res.sendFile(path.join(__dirname, '/public/html/prijava.html'));
 });
+app.get('/predmet',(req,res)=>{
+  console.log(req.session.username);
+    if(req.session.username!=null)
+    res.render("predmet", {
+        results: true
+    }); 
+    else
+    res.render("predmet", {
+        results: false
+    }); 
+    
+});
 ///////////
-app.use(express.static('public'));
+app.use(express.static(__dirname+'/public'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-var options = {
-    root: path.join(__dirname)
-};
 //post zahtjevi
+function cbCrypt(z,z1,a,res,req,k)
+{
+    if(z!=-1&&a)
+    {
+        z=-1;
+        req.session.username=k;
+        res.json({"poruka":"Uspješna prijava"});
+    }
+    else if(z==z1)
+    {
+        res.json({"poruka":"Neuspješna prijava"});
+    }
+}
 app.post('/login',function(req,res){
    let tijelo = req.body;
    fs.readFile(__dirname+"/public/data/nastavnici.json", (err, data) => {
@@ -38,33 +63,30 @@ app.post('/login',function(req,res){
         let z=0;
         for(let i=0;i<unpa.length;i++)
         {
-            bcrypt.hash(tijelo['username'], 10, function(err, hash) {
-                if(unpa[i].nastavnik.username==tijelo['username']&&unpa[i].nastavnik.userpassword==hash)
-                {
-                    res.json({"poruka":"Uspješna prijava"});
-                    z=1;
-                }
+            if(unpa[i].nastavnik.username==tijelo['username'])
+            bcrypt.compare(tijelo['password'],unpa[i].nastavnik.password_hash, function(err, rje) {
+                 if(err==null)
+                    {
+                        cbCrypt(z,unpa.length,rje,res,req,tijelo['username']);
+                    }
             });
-            if(z==1)
+            if(z==-1)
             break;
-        }
-        if(z!=1)
-        {
-            req.session.username=tijelo['username'];
-            res.json({"poruka":"Neuspješna prijava"});
         }
     }
     else 
     res.json({"poruka":"greak u fajlu "+err});
     });
-   
 });
 app.post('/logout',function(req,res){
     if(req.session.username!=null)
-    res.json({"poruka":"bio je prijavljen "});
-    req.session.username=null;
- });
- 
-
- 
+    {
+        req.session.username=null;
+        res.json({"poruka":"bio je prijavljen "});
+    }
+    else
+    res.json({"poruka":"sve 5"});
+     
+ }); 
 app.listen(3000);
+    
